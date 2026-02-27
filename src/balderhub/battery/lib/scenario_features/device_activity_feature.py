70a2c42898f394915f5b5c86f951e7a2b0690425
matosys <max@matosys.de>
2026-02-27 14:22:31 +0100

    fix(DeviceActivityFeature): add new methods `prepare()` and `cleanup()`

--- src/balderhub/battery/lib/scenario_features/device_activity_feature.py
+++ src/balderhub/battery/lib/scenario_features/device_activity_feature.py
@@ -12,16 +12,39 @@ class DeviceActivityFeature(balder.Feature):
     Scenario Level feature that allows to check if a device is active or inactive.
     """
 
+    def prepare(self) -> None:
+        """
+        Prepares the environment to be able to run the :meth:`DeviceActivityFeature.is_active` method.
+
+        .. note::
+            This method will not be called before EVERY ``is_active`` call, but when using this
+            feature you need to make sure that the :meth:`DeviceActivityFeature.is_active` call is within the
+            ``prepare()`` and ``cleanup`` call.
+        """
+
     def is_active(self) -> bool:
         """
         :return: returns True if the device is active, False otherwise
         """
         raise NotImplementedError()
 
+    def cleanup(self) -> None:
+        """
+        Cleans up the environment to be able to run the :meth:`DeviceActivityFeature.is_active` method.
+
+        .. note::
+            This method will not be called before EVERY ``is_active`` call, but when using this
+            feature you need to make sure that the :meth:`DeviceActivityFeature.is_active` call is within the
+            ``prepare()`` and ``cleanup`` call.
+        """
+
     def wait_to_be_active(self, timeout_sec: float):
         """
         This method will wait up to ``timeout_sec`` seconds until the device should be shown ACTIVE. If it does not
         do that within the ``timeout_sec`` the method will raise a TimeoutError.
+
+        .. note::
+            When using this method please make sure that it is embedded within the ``prepare()`` and ``cleanup`` call.
 
         :param timeout_sec: maximum time to wait for a device to be ACTIVE
         """
@@ -37,6 +60,9 @@ class DeviceActivityFeature(balder.Feature):
         """
         This method will wait up to ``timeout_sec`` seconds until the device should be shown INACTIVE. If it does not
         do that within the ``timeout_sec`` the method will raise a TimeoutError.
+
+        .. note::
+            When using this method please make sure that it is embedded within the ``prepare()`` and ``cleanup`` call.
 
         :param timeout_sec: maximum time to wait for a device to be INACTIVE
         """
